0da3fb8c8a89b4e37541aae44131598582b89043
DogMeatGuy <70135714+DogMeatGuy@users.noreply.github.com>
2020-11-08 19:39:56 +0300

    js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,16 @@ function nextSlide() {
     slides[currentSlide].className = 'slider-item';
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'slider-item current';
-}
+};
+
+let openModal = document.querySelector('.adress-btn');
+let modal = document.querySelector('.modal-window');
+let closeModal = modal.querySelector('.close-modal-button');
+
+openModal.onclick = function() {
+ modal.classList.add('modal-show');
+};
+
+closeModal.onclick = function() {
+ modal.classList.remove('modal-show');
+};
