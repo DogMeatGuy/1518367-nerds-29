b0ce2040202e7e9e9443f949462decfeb7898e3c
DogMeatGuy <70135714+DogMeatGuy@users.noreply.github.com>
2020-11-09 11:47:21 +0300

    d

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var slides = document.querySelectorAll('.slider-list .slider-item');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,3000);
+var slideInterval = setInterval(nextSlide,10000);
 function nextSlide() {
     slides[currentSlide].className = 'slider-item';
     currentSlide = (currentSlide+1)%slides.length;
